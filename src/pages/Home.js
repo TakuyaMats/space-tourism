9f31f0e9ebeb135008d8a92dd8ebe81f29dd7c46
Takuya Matsumoto <tmatsnj@gmail.com>
2022-05-27 17:47:06 -0400

    added material-ui package

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
 
 function Home() {
     return (
         <div>
             <h1>Hi from Home</h1>
-            
             {/* links to in NAVBAR COMPONENT, then NAVBAR COMPONENT to HOME */}
             <Link to="/">Home</Link>
             <Link to="/destination"> Destination </Link>
