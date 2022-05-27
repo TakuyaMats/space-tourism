386d2bceaecf1666c94555a92b072d0147da79cb
Takuya Matsumoto <tmatsnj@gmail.com>
2022-05-27 18:12:48 -0400

    making changes to Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 import { Link } from "react-router-dom";
 
 function Home() {
     return (
-        <div>
+        <Box className="">
             <h1>Hi from Home</h1>
             {/* links to in NAVBAR COMPONENT, then NAVBAR COMPONENT to HOME */}
-            <Link to="/">Home</Link>
+            {/* <Link to="/">Home</Link>
             <Link to="/destination"> Destination </Link>
             <Link to="/crew"> Crew </Link>
-            <Link to="/technology"> Technology </Link>
-        </div>
-
+            <Link to="/technology"> Technology </Link> */}
+        </Box>
     )
 }
 
