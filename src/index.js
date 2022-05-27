b0293e2f2209c137cdd7a1f9ac023341f237bb41
Eric Sun <ericmsun@gmail.com>
2022-05-27 17:32:54 -0400

    saving corrections to Routes in app.js, created navbar funct component

--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from './App';
 
-import Destination from './pages/Destination';
-import Home from './pages/Home';
-import Crew from './pages/Crew';
-import Technology from './pages/Technology';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element= {<App />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/destination' element= {<Destination/>}/>
-        <Route path='/crew' element= {<Crew/>}/>
-        <Route path='/technology' element= {<Technology/>}/>
-      </Routes>
+      <App/>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,9 @@ function Home() {
     return (
         <div>
             <h1>Hi from Home</h1>
+            
             {/* links to in NAVBAR COMPONENT, then NAVBAR COMPONENT to HOME */}
-            <Link to="/home">Home</Link>
+            <Link to="/">Home</Link>
             <Link to="/destination"> Destination </Link>
             <Link to="/crew"> Crew </Link>
             <Link to="/technology"> Technology </Link>
