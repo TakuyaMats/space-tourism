5ae43a9745db32246183866c9a0fb0d4635106fa
Eric Sun <ericmsun@gmail.com>
2022-05-28 13:01:12 -0400

    saving fixes to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Stack from '@mui/material/Stack'
+import Box from '@mui/material/Box';
+// import NavLogo from '../../assets/shared/logo.svg'
 
 function NavBar() {
-    
+
     return (
         <div>
-            <h1>NAVBAR</h1>
-            {/* links to in NAVBAR COMPONENT, then NAVBAR COMPONENT to HOME */}
-            <Link to="/">Home</Link>
-            <Link to="/destination"> Destination </Link>
-            <Link to="/crew"> Crew </Link>
-            <Link to="/technology"> Technology </Link>
+            <Box
+                marginTop="40px"
+                marginLeft="50px"
+                width="830px"
+                height="96px"
+            >
+                <Stack
+                    direction={{ xs: 'column', sm: 'row' }}
+                    spacing={{ xs: 1, sm: 2, md: 4 }}
+                    sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        // marginLeft: "507px"
+                    }}
+                >
+                    <Link to="/">00 Home</Link>
+                    <Link to="/destination">01 Destination </Link>
+                    <Link to="/crew">02 Crew </Link>
+                    <Link to="/technology">03 Technology </Link>
+                </Stack>
+            </Box>
         </div>
-
     )
 };
 
