7b51eec9ee280c21e4372d418846a0d55afb55b8
Takuya Matsumoto <tmatsnj@gmail.com>
2022-05-28 14:05:31 -0400

    fixed a background positioning issue and fixed container going off the screen

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,9 +9,9 @@ import backgroundImage from '../../assets/home/background-home-desktop.jpg';
 function Home() {
 
     return (
-        <Paper sx={{ backgroundImage: `url(${backgroundImage})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', width: '100vw', height: '100vh' }}>
-            <Container sx={{ position: 'absolute', width: '450px', height: '382px', left: '165px', top: '387px'}}>
-                <h5 style={{ color: '#D0D6F9', marginBottom: '0' }}>SO, YOU WANT TO TRAVEL TO</h5>
+        <Paper sx={{ backgroundImage: `url(${backgroundImage})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', width: '100vw', height: '100vh', position: 'absolute' }}>
+            <Container sx={{ position: 'relative', width: '450px', height: '382px', left: '165px', top: '387px', marginLeft: '0' }}>
+                <h5 style={{ color: '#D0D6F9', marginBottom: '0', right: '5%' }}>SO, YOU WANT TO TRAVEL TO</h5>
                 <h1 style={{ color: '#FFFFFF', lineHeight: '172px', fontStyle: 'normal', marginTop: '24px', marginBottom: '24px' }}>SPACE</h1>
                 <p style={{ mixBlendMode: 'normal' }}>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
             </Container>
