80ffa4686f60233a416a12bd91d6970e6dd7f85f
Matheus Barbosa <mattbarb93@gmail.com>
2022-05-28 01:15:00 -0400

    Updated index.css file

--- src/pages/Crew.js
+++ src/pages/Crew.js
@@ -1,13 +1,40 @@
 import React from "react";
-import NavBar from '../components/Navbar/Navbar';
+import NavBar from "../components/Navbar/Navbar";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Item from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import backgroundImage from "../assets/crew/background-crew-desktop.jpg";
 
 function Crew() {
-    return (
-        <div>
-            <NavBar/>
-            <h1>MEET YOUR CREW</h1>
-        </div>
-    )
+  return (
+    <Paper
+      sx={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      {/* <Container>
+        <Grid>Info</Grid>
+        <Grid>Image</Grid>
+      </Container> */}
+      <Container maxWidth="sm">
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <h3>CONTENT</h3>
+          </Grid>
+          <Grid item xs={6}>
+            IMAGE
+          </Grid>
+        </Grid>
+      </Container>
+    </Paper>
+  );
 }
 
 export default Crew;
