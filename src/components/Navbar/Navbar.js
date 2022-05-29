0a1bc5b042944af22c60b83b607b0c31d5951f3e
Eric Sun <ericmsun@gmail.com>
2022-05-29 19:42:12 -0400

    saving changes to navbar, resolved props bug in React Dom

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,74 +6,41 @@ import { Link } from "react-router-dom";
 import Container from "@mui/material/Container";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import Box from "@mui/material/Box";
-import Menu from "@mui/icons-material/Menu";
-import MenuItem from '@mui/material/MenuItem';
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+
+// import Box from "@mui/material/Box";
+// import Menu from "@mui/icons-material/Menu";
+// import MenuItem from '@mui/material/MenuItem';
 import Button from "@mui/material/Button";
 import Icon from "@mui/material/Icon";
 import NavLogo from '../../assets/shared/logo.svg'
-
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import CloseIcon from '@mui/icons-material/Close';
 // install material ui icons for imports below
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from '@mui/icons-material/Menu';
 
 
+
 function NavBar() {
-    const [anchorElNav, setAnchorElNav] = useState(null);
-    
+    const [open, setOpen] = useState(false)
 
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-    };
-
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
 
     return (
-        <AppBar position='static' sx={{ marginTop: '40px' }}>
-            <Container maxWidth='xl'>
+        <AppBar position='static' sx={{ marginTop: 4 }}>
+            <Container maxWidth='lg'>
                 <Toolbar disableGutters>
-                    <Icon sx={{ display: { xs: 'flex', md: 'flex'}, ml: 2 }} >
+
+                    {/* SPACE ICON */}
+
+                    <Icon sx={{ display: { xs: 'flex', md: 'flex' }, ml: 2, marginRight: 'auto' }} >
                         <img src={NavLogo} />
                     </Icon>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                        {/* <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton> */}
-                        <Menu
-                            id="menu-appbar"
-                            // anchorEl={anchorElNav}
-                            // anchorOrigin={{
-                            //     vertical: 'bottom',
-                            //     horizontal: 'left',
-                            // }}
-                            // keepMounted
-                            // transformOrigin={{
-                            //     vertical: 'top',
-                            //     horizontal: 'left',
-                            // }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: 'block', md: 'none' },
-                            }}
-                        >
-                            <MenuItem onClick={handleCloseNavMenu}><Link to="/">00 Home</Link></MenuItem>
-                            <MenuItem onClick={handleCloseNavMenu}><Link to="/destination">01 Destination</Link></MenuItem>
-                            <MenuItem onClick={handleCloseNavMenu}><Link to="/crew">02 Crew</Link></MenuItem>
-                            <MenuItem onClick={handleCloseNavMenu}><Link to="/technology">03 Technology</Link></MenuItem>
-                        </Menu>
-                    </Box>
 
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end' }}>
+                    {/* NAVBAR LINKS */}
+
+                    <Container sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end' }}>
                         <Button component={Link} to="/" sx={{ my: 2, color: 'white', display: 'block' }} >00 Home</Button>
                         <Button component={Link} to="/destination" sx={{ my: 2, color: 'white', display: 'block' }}>01 Destination</Button>
                         <Button component={Link} to="/crew" sx={{ my: 2, color: 'white', display: 'block' }}>02 Crew</Button>
@@ -84,10 +51,55 @@ function NavBar() {
                         <Link onClick={handleCloseNavMenu} to="/crew">02 Crew</Link>
                         <Link onClick={handleCloseNavMenu} to="/technology">03 Technology</Link> */}
 
-                    </Box>
+                    </Container>
 
+
+                    {/*  HAMBURGER MENU */}
+
+
+                    <Container sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, justifyContent: 'flex-end' }}>
+
+                        {/* hamburger menu button icon */}
+                        <IconButton onClick={() => setOpen(true)}>
+                            <MenuIcon />
+                        </IconButton>
+                        {/* */}
+                    </Container>
                 </Toolbar>
             </Container>
+            <SwipeableDrawer 
+            anchor='right' 
+            open={open} 
+            onOpen={() => setOpen(true)} 
+            onClose={() => setOpen(false)} >
+                <div style={{ display: 'flex', justifyContent:'flex-end'}}>
+                    <IconButton onClick={() => setOpen(false)}>
+                        <CloseIcon />
+                    </IconButton>
+                </div>
+                <List>
+                    <ListItem>
+                        <Button component={Link} to="/">
+                            00 Home
+                        </Button>
+                    </ListItem>
+                    <ListItem>
+                        <Button component={Link} to="/destination" >
+                            01 Destination
+                        </Button>
+                    </ListItem>
+                    <ListItem>
+                        <Button component={Link} to="/crew" >
+                            02 Crew
+                        </Button>
+                    </ListItem>
+                    <ListItem>
+                        <Button component={Link} to="/technology">
+                            03 Technology
+                        </Button>
+                    </ListItem>
+                </List>
+            </SwipeableDrawer>
         </AppBar>
     )
 };
