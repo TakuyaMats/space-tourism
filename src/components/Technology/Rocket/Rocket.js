0fd1c513b6fa78f10b61acfdbc33b56744715641
Takuya Matsumoto <tmatsnj@gmail.com>
2022-06-04 16:31:06 -0400

    got the information rendering

--- src/components/Technology/Rocket/Rocket.js
+++ src/components/Technology/Rocket/Rocket.js
@@ -1,12 +1,25 @@
 import React from "react";
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 function Rocket({ data }) {
 
-    console.log(data[0].name);
+    let capitalize = (data) => {
+        let arrName = [];
+
+        data.forEach(info => {
+            let capital = info.name.toUpperCase();
+            arrName.push(capital)
+        })
+        return arrName;
+    }
+
     return (
-        <div>
-            <h1>Rocket Component</h1>
-        </div>
+        <Container>
+            <Typography>THE TERMINOLOGY...</Typography>
+            <Typography>{capitalize(data)[0]}</Typography>
+            <Typography>{data[0].description}</Typography>
+        </Container>
     )
 }
 
