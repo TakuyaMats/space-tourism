0acdb1473a38980f0a809d687644a67f17eeefa7
Eric Sun <ericmsun@gmail.com>
2022-06-06 13:27:44 -0400

    saving .map for planet info

--- src/pages/Destination/Destination.js
+++ src/pages/Destination/Destination.js
@@ -1,13 +1,11 @@
 import React from "react";
-
-import PlanetInfo from '../../components/Destination/Tabs/Tabs';
-
+import PlanetInfo from '../../components/Destination/PlanetInfo/PlanetInfo';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './Theme';
+import { data } from '../../assets/dataJSON';
 
 import NavBar from '../../components/Navbar/Navbar';
-
-import Box from '@mui/material/Box';
+// import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
@@ -15,6 +13,7 @@ import Container from '@mui/material/Container';
 
 
 function Destination() {
+    const { Destinations } = data;
     return (
         <ThemeProvider theme={theme}>
             <Paper variant="body1" square>
@@ -25,13 +24,13 @@ function Destination() {
                     </Typography>
                 </Container>
                 <Container sx={{ borderStyle: 'groove', borderColor: 'red', display: 'flex', justifyContent: 'space-around' }}>
-                    <Box sx={{ borderStyle: 'groove', borderColor: 'yellow', display: 'flex' }}>
+                    {/* <Box sx={{ borderStyle: 'groove', borderColor: 'yellow', display: 'flex' }}>
                         <Typography sx={{ width: '445px', height: '445px' }}>
                             Planet IMG
                         </Typography>
-                    </Box>
-                    <PlanetInfo styles={{}} />
-                </Container>
+                    </Box> */}
+                    <PlanetInfo data={Destinations} />
+                </Container> 
             </Paper>
         </ThemeProvider>
     )
