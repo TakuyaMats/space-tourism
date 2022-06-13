bfb20ebae94ef8200ac5c2a9c4e9c501e289eb6d
Eric Sun <ericmsun@gmail.com>
2022-06-12 21:47:12 -0400

    saving relative positioning for info contianer, beginning of media queries

--- src/components/Destination/PlanetInfo/PlanetInfo.js
+++ src/components/Destination/PlanetInfo/PlanetInfo.js
@@ -6,11 +6,9 @@ import moonImg from '../../../assets/destination/image-moon.webp'
 import europaImg from '../../../assets/destination/image-europa.webp'
 import marsImg from '../../../assets/destination/image-mars.webp'
 import titanImg from '../../../assets/destination/image-titan.webp'
-// import { grey } from '@mui/material/colors';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
-import Tabs from '@mui/material/Tabs'
 import Typography from '@mui/material/Typography';
 import Tab from '@mui/material/Tab';
 import Container from '@mui/material/Container';
@@ -68,12 +66,10 @@ function PlanetInfo({ data }) {
                             value={value}
                             onChange={handleChange}
                         >
-
                             <Tab sx={{ color: '#D0D6F9' }} variant='bttn' value='Moon' onClick={handleClick} label="Moon" />
                             <Tab sx={{ color: '#D0D6F9' }} value='Mars' onClick={handleClick} label="Mars" />
                             <Tab sx={{ color: '#D0D6F9' }} value='Europa' onClick={handleClick} label="Europa" />
                             <Tab sx={{ color: '#D0D6F9' }} value='Titan' onClick={handleClick} label="Titan" />
-
                         </TabList>
                     </Container>
                     {data.map((planet, index) => {
